a10d754979e94164c6a3faf2591c60b6e680114f
Soham Jadhav <sohamjadhavth@gmail.com>
2026-02-21 16:01:37 +0530

    initial commit

---
+++ template.sh
@@ -0,0 +1,15 @@
+#create directory
+mkdir -p src
+mkdir -p research
+
+#create file
+touch src/__init.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch sertup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Successfully done!!"
